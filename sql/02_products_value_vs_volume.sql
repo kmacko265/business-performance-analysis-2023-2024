b40daf3e60786c2fab94aeb0bf6939eb00b44cf9
kmacko265 <kmacko265@gmail.com>
2026-01-26 17:03:28 +0100

    Update 02_products_value_vs_volume.sql

--- sql/02_products_value_vs_volume.sql
+++ sql/02_products_value_vs_volume.sql
@@ -1,1 +1,13 @@
-2_products_value_vs_volume.sql
+-- Day 2: Product Value vs Volume
+-- Goal: Identify volume-driven vs value-driven products
+-- Metrics: units sold (COUNT(*)), revenue (SUM(sale_price))
+
+SELECT
+  p.name,
+  COUNT(*) AS units_sold,
+  ROUND(SUM(oi.sale_price),2) AS revenue
+FROM `practice-yooo.portfolio_practice.order_items` AS oi
+JOIN `practice-yooo.portfolio_practice.products` AS p
+  ON oi.product_id = p.id
+GROUP BY p.name
+ORDER BY revenue DESC;
